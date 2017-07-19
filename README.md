1ab1c1cf0fc385952ca7c814a11b8d2bda64a195
John Hanna <jph_jr@yahoo.com>
2017-07-19 18:18:50 +0000

    Initial commit of converted project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# simon-fcc
-Simon game project for freeCodeCamp
+# simon-jquery
+Simon game project for freeCodeCamp converted from vanilla JS to jQuery
 
 Front-end development project for freeCodeCamp that should play similarly to the original Simon game.
 It has a *strict* mode which, when enabled, will start game over when you make an error.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,145 +1,143 @@
-var sequence = []; // array to hold sequence
-var playerSequence = 0; // Count position in sequence player is testing
-var playerGo = false; // Player's turn?
-var computerGo = true; // Computer's turn?
-var round = 0; // counter on display
-var strict = false; // Strict game?
-var startButton = document.getElementById("startButton");
-var strictButton = document.getElementById("strictButton");
-var buttons = document.querySelector(".pad");
-var roundDisplay = document.getElementById("round");
-var youWin = document.getElementById("youWin");
-var yourTurn = document.getElementById("yourTurn");
-var tryAgain = document.getElementById("tryAgain");
-var strictError = document.getElementById("strictError");
-startButton.addEventListener("click", start);
-strictButton.addEventListener("click", () => {
-  strict = !strict;
-  if (strict) {
-    strictButton.style.background = "#b71c1c";
-  } else {
-    strictButton.style.background = "#2c3e50";
-  }
-});
-buttons.addEventListener("click", (event) => {
-  if (event.target.id === "b1" ||
-      event.target.id === "b2" ||
-      event.target.id === "b3" ||
-      event.target.id === "b4") {
-        playersTurn(event);
-  }
-});
-
-/* Simon chooses next number to add to sequence and runs the round*/
-function getSimon() {
-  round++;
-  roundDisplay.innerText = addZero(round);
-  sequence.push(Math.floor(Math.random() * (5 - 1)) + 1);
-  activate(sequence);
-}
-
-function addZero(num) {
-  return (num < 10) ? "0" + num : num;
-}
-
-/* Simon show sequence */
-function activate(sequence) {
-  var i = 0;
-  var timing = setInterval(() => {
-    lightButton(sequence[i]);
-    i++;
-    if (i >= sequence.length) {
-      clearInterval(timing);
-      computerGo = false;
-      playerGo = true;
-      setTimeout(() => {yourTurn.style.display = "initial"}, 700);
+$(document).ready(function() {
+  var sequence = []; // array to hold sequence
+  var playerSequence = 0; // Count position in sequence player is testing
+  var playerGo = false; // Player's turn?
+  var computerGo = true; // Computer's turn?
+  var round = 0; // counter on display
+  var strict = false; // Strict game?
+  var $startButton = $("#startButton");
+  var $strictButton = $("#strictButton");
+  var $buttons = $(".pad");
+  var $roundDisplay = $("#round");
+  var $youWin = $("#youWin");
+  var $yourTurn = $("#yourTurn");
+  var $tryAgain = $("#tryAgain");
+  var $strictError = $("#strictError");
+  $startButton.on("click", start);
+  $strictButton.on("click", () => {
+    strict = !strict;
+    if (strict) {
+      $strictButton.css({background: "#b71c1c"});
+    } else {
+      $strictButton.css({background: "#2c3e50"});
     }
-  }, 700);
-}
-/* Simon button activation */
-function lightButton(number) {
-  var sound = document.getElementById("tone" + number);
-  var button = document.getElementById("b" + number);
-  sound.play();
-  button.classList.remove("off");
-  button.classList.add("on");
-  setTimeout(() => {
-    button.classList.remove("on");
-    button.classList.add("off");
-  }, 350);
-}
+  });
+  $buttons.on("click", (event) => {
+    if (event.target.id === "b1" ||
+        event.target.id === "b2" ||
+        event.target.id === "b3" ||
+        event.target.id === "b4") {
+          playersTurn(event);
+    }
+  });
 
-function playersTurn(event) {
-  if (playerGo === true) {
-    var playerButton = event;
-    bleep(playerButton);
+  /* Simon chooses next number to add to sequence and runs the round*/
+  function getSimon() {
+    round++;
+    $roundDisplay.text(addZero(round));
+    sequence.push(Math.floor(Math.random() * (5 - 1)) + 1);
+    activate(sequence);
   }
-}
 
-/* Player button activation */
-function bleep(e) {
-  if (playerGo === true) {
-    var number = Number(e.target.dataset.number);
-    var sound = document.getElementById("tone" + number);
-    sound.currentTime = 0;
-    sound.play();
-    buttons.removeEventListener("click", bleep);
-    e.target.classList.remove("off");
-    e.target.classList.add("on");
-    testSeq(number);
+  function addZero(num) {
+    return (num < 10) ? "0" + num : num;
+  }
+
+  /* Simon show sequence */
+  function activate(sequence) {
+    var i = 0;
+    var timing = setInterval(() => {
+      lightButton(sequence[i]);
+      i++;
+      if (i >= sequence.length) {
+        clearInterval(timing);
+        computerGo = false;
+        playerGo = true;
+        setTimeout(() => {$yourTurn.css({display: "initial"})}, 700);
+      }
+    }, 700);
+  }
+  /* Simon button activation */
+  function lightButton(number) {
+    var $sound = $("#tone" + number);
+    var $button = $("#b" + number);
+    $sound.get(0).play();
+    $button.removeClass("off").addClass("on");
     setTimeout(() => {
-      e.target.classList.remove("on");
-      e.target.classList.add("off");
-      buttons.addEventListener("click", bleep);
+      $button.removeClass("on").addClass("off");
     }, 350);
   }
-}
 
-/* Test player sequence */
-function testSeq(number) {
-  if (number === sequence[playerSequence]) {
+  function playersTurn(event) {
+    if (playerGo === true) {
+      var playerButton = event;
+      bleep(playerButton);
+    }
+  }
+
+  /* Player button activation */
+  function bleep(e) {
+    var element = $(e.target);
+    if (playerGo === true) {
+      var number = Number(e.target.dataset.number);
+      var $sound = $("#tone" + number);
+      $sound.currentTime = 0;
+      $sound.get(0).play();
+      $buttons.off("click", bleep);
+      element.removeClass("off").addClass("on");
+      testSeq(number);
+      setTimeout(() => {
+        element.removeClass("on").addClass("off");
+        $buttons.on("click", bleep);
+      }, 350);
+    }
+  }
+
+  /* Test player sequence */
+  function testSeq(number) {
+    if (number === sequence[playerSequence]) {
       playerSequence++;
       if (playerSequence === sequence.length) {
         if (playerSequence === 20) {
-          youWin.style.display = "initial";
+          $youWin.css({display: "initial"});
           playerGo = false;
           computerGo = true;
-          setTimeout(() => {youWin.style.display = "none"; start();}, 2000);
+          setTimeout(() => {$youWin.css({display: "none"}); start();}, 2000);
         } else {
           playerSequence = 0;
           playerGo = false;
           computerGo = true;
-          yourTurn.style.display = "none";
+          $yourTurn.css({display: "none"});
           setTimeout(getSimon, 1000);
         }
       }
-  } else {
-    if (strict) {
-      strictError.style.display = "initial";
-      yourTurn.style.display = "none";
     } else {
-      yourTurn.style.display = "none";
-      tryAgain.style.display = "initial";
-      playerGo = false;
-      computerGo = true;
-      playerSequence = 0;
-      setTimeout(() => {tryAgain.style.display = "none"; activate(sequence)}, 2000);
+      if (strict) {
+        $strictError.css({display: "initial"});
+        $yourTurn.css({display: "none"});
+      } else {
+        $yourTurn.css({display: "none"});
+        $tryAgain.cs({display: "initial"});
+        playerGo = false;
+        computerGo = true;
+        playerSequence = 0;
+        setTimeout(() => {$tryAgain.css({display: "none"}); activate(sequence)}, 2000);
+      }
     }
   }
-}
 
-function start() {
-  sequence = [];
-  round = 0;
-  playerSequence = 0;
-  strictError.style.display = "none";
-  yourTurn.style.display = "none";
-  tryAgain.style.display = "none";
-  youWin.style.display = "none";
-  roundDisplay.innerText = "00";
-  setTimeout(getSimon, 1000);
-}
-
+  function start() {
+    sequence = [];
+    round = 0;
+    playerSequence = 0;
+    $strictError.css({display: "none"});
+    $yourTurn.css({display: "none"});
+    $tryAgain.css({display: "none"});
+    $youWin.css({display: "none"});
+    $roundDisplay.text("00");
+    setTimeout(getSimon, 1000);
+  }
+});
 
 /* BEGIN GAME
   and if player gets sequence correct,
